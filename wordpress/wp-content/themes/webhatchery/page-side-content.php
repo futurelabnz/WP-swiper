7de18f7e64dddae9a096ec2278b5412db7e8a9a4
Clara <clara@futurelab.co.nz>
2016-01-18 17:57:55 +1300

    -slider

--- wordpress/wp-content/themes/webhatchery/page-side-content.php
+++ wordpress/wp-content/themes/webhatchery/page-side-content.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: s02 Side Content
+ * Template Name: s02 Side Content - 
  *
  * @package WordPress
  * @subpackage Webhatchery
@@ -10,14 +10,17 @@ $gallery = get_field('gallery', $child->ID);
 ?>
 <div class="slide1">
 	<div class="row">
-		<div class="col-md-6">
-			<img class="slide-image" src="<?php echo $gallery[0]['url'];?>">	
+		<div class="col-md-6 columns">
+			<img class="slide-image one" src="<?php echo $gallery[0]['url'];?>">	
+			<img class="slide-image two" src="<?php echo $gallery[1]['url'];?>">	
+			<img class="slide-image three" src="<?php echo $gallery[2]['url'];?>">	
+			<img class="slide-image four" src="<?php echo $gallery[3]['url'];?>">
 		</div>
-		<div class="col-md-6">
-			<p>
+		<div class="col-md-6 columns">
+			<h2>
 			   <?php echo $child->post_title; ?>
-			</p>
-      <p>
+			</h2>
+      		<p class="home-content-text">
 			   <?php echo $child->post_content; ?>
 			</p>
 		</div>
