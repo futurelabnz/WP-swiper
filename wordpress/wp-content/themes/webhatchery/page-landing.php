99c9c1d78810071aefa010d4795d6fee7a5d1e05
pojke <ralf@futurelab.co.nz>
2016-01-18 16:49:04 +1300

    added content to the pages

--- wordpress/wp-content/themes/webhatchery/page-landing.php
+++ wordpress/wp-content/themes/webhatchery/page-landing.php
@@ -5,12 +5,15 @@
  * @package WordPress
  * @subpackage Webhatchery
  */
-$gallery = get_field('gallery');
+$gallery = get_field('gallery',$page->ID);
 ?>
 
 <div class="slide1">
   <img class="logo" src="<?php echo $gallery[0]['url'];?>">
   <p>
     <?php echo $page->post_title; ?>
+  </p>
+  <p>
+    <?php echo $page->post_content; ?>
   </p>
 </div>
