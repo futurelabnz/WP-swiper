2d945c4f1544bc854a0ff01eb2eabefaf70bed5f
Clara <clara@futurelab.co.nz>
2016-01-22 17:58:18 +1300

    Why use Wordpress ?

--- wordpress/wp-content/themes/webhatchery/page-internal-slider.php
+++ wordpress/wp-content/themes/webhatchery/page-internal-slider.php
@@ -8,16 +8,14 @@
 $gallery = get_field('gallery', $page->ID);
 ?>
 
-<div class="internal-slider" style="width: 850px; height: 200px;">
+<div class="internal-slider" style="width: 850px; height:auto;">
   <h2 class="reason-title">
     <?php echo $child->post_title; ?>
   </h2>
 
-  <div class="row">
-    <!-- <div class="col-md-6">
-      <img class="" src="<?php echo $gallery[0]['url']; ?>">
-    </div> -->
-    <div class="col-md-12">
+
+
+
     <?php $subchildren = get_pages(array('parent' => $child->ID, 'sort_column' => 'post_date')); ?>
     <?php if (!empty($subchildren)): ?>
       <div class="swiper-container swiper-subchild">
@@ -26,7 +24,6 @@ $gallery = get_field('gallery', $page->ID);
             <div class="swiper-slide xxxxx">
               <?php include(locate_template(get_page_template_slug($subchild->ID))); ?>
             </div>
-            
           <?php endforeach; ?>
         </div>
         <div class="swiper-subchild-next"></div>
@@ -34,11 +31,12 @@ $gallery = get_field('gallery', $page->ID);
       </div>
     <?php endif; ?>
 
+
+    <div class="col-md-12">
       <p>
         <?php echo $child->post_content; ?>
       </p>
     </div>
-  </div>
 </div>
 
-</div>
+
