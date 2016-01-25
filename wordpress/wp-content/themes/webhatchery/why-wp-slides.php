68f33e9f9d5e4890956dbc0089ce302687b69fe3
Clara <clara@futurelab.co.nz>
2016-01-25 17:35:27 +1300

    why wordpress animation

--- wordpress/wp-content/themes/webhatchery/why-wp-slides.php
+++ wordpress/wp-content/themes/webhatchery/why-wp-slides.php
@@ -10,14 +10,11 @@ $gallery = get_field('gallery',$subchild->ID);
 ?>
 
 
-<div class="row">
-
+<div class="row text-box">
   <div class="col-sm-6">
 <h3 class="mobile_only"><?php echo $subchild->post_title; ?></h3>
-
-    <img class="icons" src="<?php echo $gallery[0]['url'];?>"></div>
+  <img class="icons" src="<?php echo $gallery[0]['url'];?>"></div>
   <div class="col-sm-6 t-l-desktop">
     <h3 class="desktop_only"><?php echo $subchild->post_title; ?></h3>
     <p><?php echo $subchild->post_content; ?></p></div>
-
-</div>
+  </div>
